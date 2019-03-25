e012af7a63f12f09187ed8eed938c2bc79c5bd7c
Sameeh <sameehkhan1@gmail.com>
2019-03-25 14:17:14 -0700

    Update entry file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from 'store';
+import Root from './components/root';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
 
 document.addEventListener('DOMContentLoaded', () => {
-    const root = document.getElementById('root');
-    ReactDOM.render(<App />, root);
-});
+    const store = configureStore();
 
-serviceWorker.unregister();
+    // TESTING START
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    // TESTING END
+
+    const root = document.getElementById('root');
+    ReactDOM.render(<Root store={store} />, root);
+});
